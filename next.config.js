529f8050eab028146318df50806bf1b1080fbf3a
alfredotang <asdzxcca0617@gmail.com>
2022-04-21 02:59:36 +0800

    chore: add analyze setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const composePlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = composePlugins(
+  [
+    withBundleAnalyzer({
+      enabled: process.env.ANALYZE === 'true',
+    }),
+  ],
+  nextConfig
+)
